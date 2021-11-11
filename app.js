87022cac949570411f099bfbee003810aa643ab1
Sergey <example@gmail.com>
2021-11-11 21:24:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+//UDEMY !!!!
+//'use strict'
+//a = 15
+//console. log (a);
+//Типы данных
+
+//Обьекты 
+
+//const obj = {
+  //  name: 'John',
+    //age: 28, 
+    //isMarried: false 
+
+//};
+
+//console.log(obj.name); //правильнее так 
+
+//console.log(obj['name']); //но можна и так 
+
+//Массивы 
+
+//let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {},[]];
+
+//console.log(arr[1]);
+
+//Урок № 7 Простое общение с пользоватилем
+
+'use strict';
+
+//alert('Hello');
+
+//const result = confirm('Are you hear?');
+
+//const answer = + prompt ('Тебе есть 18', '18'); //Преобразование строки в число с помощью оператора + 
+//console.log((answer + 5));
+
+//const answers = [];
+//answers [0] = prompt ('Введите Ваше имя');
+//answers [1] = prompt ('Введите Вашу фамилию');
+//answers [2] = prompt ('Введите Ваш возраст');
+
+//document.write(answers);
+//console.log(typeof(answers));
+
+//Урок № 8 Интерполяция
+
+//const category = 'toys';
+//console.log(`http://someurl.com/${category}`);
+
+//const user = 'Ivan';
+//alert (`hi ${user}`);
+
+//Урок № 9 Операторы
+
+//let incr = 10,
+  //  decr = 10;
+
+    //incr ++,
+    //decr --;
+    
+    //console.log(++incr);
+    //console.log(--decr);
+    
+//console.log(5%2);
+   // console.log(4*2 == '8');
+
+
+// const isCheked = true,
+  //    isClose = false;
+      
+    //  console.log(isCheked || isClose);
+    
